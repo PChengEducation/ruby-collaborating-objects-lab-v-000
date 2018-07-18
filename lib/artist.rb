8d4925296d7e0d17332b54da1ffc2124afa90649
Pan Cheng <pchengeducation@hotmail.com>
2018-07-18 19:35:49 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -42,7 +42,8 @@ class Artist
     end
 
     def print_songs #instance method iterates through @songs of an instance of Artist
-      puts self.songs.each {|song| song.name}
+     self.songs.each do |song| 
+       puts song.name
 
     end
   end
