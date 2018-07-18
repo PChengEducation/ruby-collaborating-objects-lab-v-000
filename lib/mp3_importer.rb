788f38cd5eaddb47858c6802e4ed3ce37077513c
Pan Cheng <pchengeducation@hotmail.com>
2018-07-18 19:29:02 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,17 +1,21 @@
 class MP3Importer
 
-attr_accessor :path, :name
+attr_accessor :path
 
-def intialize(name)
-  @name = name
+def intialize(path)
+  @path = path
 end
 
 def files
-
+files = []
+Dir.new(self.path).each do |file|
+  files << file if file.length > 4
+end
+files
 end
 
 def import
-
+self.
 end
 
 
